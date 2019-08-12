105e3070ed41d89ec9eb9219554add837c449785
RedJanvier <janvierntwali@gmail.com>
2019-08-12 04:02:06 +0200

    added frontend as public

---
+++ public/precache-manifest.9eb2240eefc5187ca3edd83fa093663f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b58255499a080bdceee4c2ad1b4a6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d8aaf4cbed23f41f771",
+    "url": "/static/js/2.ee39b639.chunk.js"
+  },
+  {
+    "revision": "e143999d7c19176559fd",
+    "url": "/static/js/main.535329ab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5588d35c61af2963be14b074990f4b61",
+    "url": "/static/media/icon.5588d35c.png"
+  }
+]);
