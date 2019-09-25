03dc080bea78eac2ae627fbfb0d5e60044e004d6
RedJanvier <janvierntwali@gmail.com>
2019-09-25 22:40:44 +0200

    added some jwt on delete and edit routes

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,29 +1,46 @@
+const jwt = require('jsonwebtoken');
+
 const handleSignIn = (db, bcrypt) => (req, res) => {
-    const { email, password } = req.body;
+    const {
+        email,
+        password
+    } = req.body;
     const validateEmail = (vemail) => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(vemail).toLowerCase());
     };
-    if(validateEmail(email)){
+    if (validateEmail(email)) {
         db.select('email', 'hash')
-        .from('login')
-        .where('email', '=', email)
-        .returning('*')
-        .then(data => {
-            const isValid = bcrypt.compareSync( password, data[0].hash )
-            if( isValid ){
-                db.select('*')
-                .from('members')
-                .where( 'email', '=', email )
-                .then(user => {
-                    res.json(user[0])
-                })
-                .catch(err => res.status(404).json('user not found'));
-            }else{
-                res.status(400).json('wrong credentials');
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
+            .from('login')
+            .where('email', '=', email)
+            .returning('*')
+            .then(data => {
+                const isValid = bcrypt.compareSync(password, data[0].hash)
+                if (isValid) {
+                    db.select('*')
+                        .from('members')
+                        .where('email', '=', email)
+                        .then(user => {
+                            const token = jwt.sign(user[0], "elite-members-secret", {
+                                expiresIn: "2h"
+                            });
+                            return res.status(200).json({
+                                message: "you are successfully logged in",
+                                token
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(404).json('user not found')
+                        });
+                } else {
+                    res.status(403).json('wrong credentials! FORBIDEN');
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(403).json('wrong credentials! FORBIDEN')
+            })
     }
 }
 
