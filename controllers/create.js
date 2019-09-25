9018a5966c17be26ce7ff4e2f3ee61a44419e653
RedJanvier <janvierntwali@gmail.com>
2019-09-25 23:39:03 +0200

    added some console logs of acts being made on create edit and delete routes

--- controllers/create.js
+++ controllers/create.js
@@ -1,45 +1,57 @@
 const handleCreate = (db, bcrypt) => (req, res) => {
-    const { name, email, password, location, committee } = req.body;
-    if(name, email){
-        if(password && committee){
+    const {
+        name,
+        email,
+        password,
+        location,
+        committee
+    } = req.body;
+    if (name, email) {
+        if (password && committee) {
             const hash = bcrypt.hashSync(password);
-            db('login').insert({ email, hash })
-            .returning('email')
-            .then(eEmail => {
-                return db('members').insert({
+            db('login').insert({
+                    email,
+                    hash
+                })
+                .returning('email')
+                .then(eEmail => {
+                    return db('members').insert({
+                            name,
+                            email: eEmail[0],
+                            location,
+                            committee,
+                            joined: new Date()
+                        })
+                        .returning('*')
+                        .then(member => {
+                            console.log(req.userData.email + 'created a new user: ' + member[0].email);
+                            res.json(member[0])
+                        })
+                }).catch(err => {
+                    console.log(err);
+                    res.status(400).json({
+                        message: 'Unable to register user'
+                    });
+                });
+        } else {
+            return db('members').insert({
                     name,
-                    email: eEmail[0],
+                    email,
                     location,
-                    committee,
                     joined: new Date()
                 })
                 .returning('*')
                 .then(member => {
+                    console.log(req.userData.email + 'created a new user: ' + member[0].email);
                     res.json(member[0])
                 })
-                .catch(err => res.status(400).json('Unable to register user'));
-            })
-            .catch(err => res.status(400).json('Unable to register user'));
-        }else{
-            return db('members').insert({
-                name,
-                email,
-                location,
-                joined: new Date()
-            })
-            .returning('*')
-            .then(member => {
-                res.json(member[0])
-            })
-            .catch(err => res.status(400).json('Unable to register user'))
+                .catch(err => res.status(400).json('Unable to register user'))
         }
-    }else{
+    } else {
         res.status(400).json("unable to register");
     }
 }
 
-module.exports ={
+module.exports = {
     handleCreate
 }
-
-
--- controllers/delete.js
+++ controllers/delete.js
@@ -1,32 +1,42 @@
 const handleDelete = (db) => (req, res) => {
-    const { id } = req.params;
-    const { email } = req.body;
+    const {
+        id
+    } = req.params;
+    const {
+        email
+    } = req.body;
     const validateEmail = (vEmail) => {
-        // const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        // return re.test(String(vEmail).toLowerCase());
-        return true
+        const re = /^([\w]+)@([a-zA-Z]{2,12})+\.(([a-zA-Z]{2,5})+)(\.[a-zA-Z]{2,5})*$/;
+        return re.test(String(vEmail).toLowerCase());
     }
-    if(id > 0 && validateEmail(email)){
+    if (id > 0 && validateEmail(email)) {
         db('members')
-        .where({ id: id , email: email })
-        .del()
-        .returning('*')
-        .then((user) => {
-            if(user[0].committee && validateEmail(user[0].email)){
-                db('login')
-                .where('email', '=', user[0].email)
-                .del()
-                .returning('email')
-                .then(rEmail => res.json(user[0].email + ' deleted password'))
-                .catch(err => res.status(400).json("user doesn't have password"))
-            }else if(validateEmail(user[0].email)){
-                res.json(user[0].email + ' was successfully deleted')
-            }else{
-                res.status(404).json('user not found')
-            }
-        })
-        .catch(err => res.status(404).json('user not found'))
-    }else{
+            .where({
+                id: id,
+                email: email
+            })
+            .del()
+            .returning('*')
+            .then((user) => {
+                console.log(`${req.userData.email} just deleted ${user[0].email}`);
+                if (user[0].committee && validateEmail(user[0].email)) {
+                    db('login')
+                        .where('email', '=', user[0].email)
+                        .del()
+                        .returning('email')
+                        .then(rEmail => res.json(user[0].email + ' deleted password'))
+                        .catch(err => res.status(400).json("user doesn't have password"))
+                } else if (validateEmail(user[0].email)) {
+                    res.json(user[0].email + ' was successfully deleted')
+                } else {
+                    res.status(404).json('user not found');
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(404).json('member not found');
+            })
+    } else {
         res.status(400).json('Failed to delete ' + email)
     }
 }
