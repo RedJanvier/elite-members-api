e8ee2f768b64290f98c85f2bc7bdae04a77f1f10
RedJanvier <janvierntwali@gmail.com>
2019-09-29 02:33:49 +0200

    temporarily disabling chechauth to create and admin for the site

--- routes/routes.js
+++ routes/routes.js
@@ -8,7 +8,7 @@ router.use(cors());
 const member_handle = require('../controllers/members');
 
 router.get('/', (req, res, next) => member_handle.init(req, res));
-router.post('/create', checkAuth, (req, res, next) => member_handle.handleCreate(req, res));
+router.post('/create', (req, res, next) => member_handle.handleCreate(req, res));
 router.put('/edit/:id', checkAuth, (req, res, next) => member_handle.handleEdit(req, res));
 router.post('/signin', (req, res, next) => member_handle.handleSignIn(req, res));
 router.delete('/member/:id', checkAuth, (req, res, next) => member_handle.handleDelete(req, res));
